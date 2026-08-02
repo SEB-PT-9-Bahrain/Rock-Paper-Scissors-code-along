dbe3bd59ca1c8183055d8da3c47b0f6d61f39653
omarakamal <omaraakamal@gmail.com>
2026-08-02 18:47:04 +0300

    feat: add event listener to elements

--- js/app.js
+++ js/app.js
@@ -5,7 +5,20 @@ let playerChoice = null
 let computerChoice = null
 let message = null
 /*------------------------ Cached Element References ------------------------*/
+const rockElement = document.querySelector('#rock')
+const paperElement = document.querySelector('#paper')
+const scissorsElement = document.querySelector('#scissors')
 
+const allChoiceElements = document.querySelectorAll('.choice-el')
+
+console.log(allChoiceElements[2])
 /*-------------------------------- Functions --------------------------------*/
+function play(){
+    console.log('clicked on choice')
 
+}
 /*----------------------------- Event Listeners -----------------------------*/
+
+for(let oneChoiceElement of allChoiceElements){
+    oneChoiceElement.addEventListener('click',play)
+}
