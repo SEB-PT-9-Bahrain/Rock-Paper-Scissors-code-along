5da0d4d1e76294423b9e6c5027bbcc578f154de4
omarakamal <omaraakamal@gmail.com>
2026-08-02 18:31:10 +0300

    feat: define the states of the game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const choices = ['rock','paper','scissors']
 /*-------------------------------- Variables --------------------------------*/
-
+let playerChoice = null
+let computerChoice = null
+let message = null
 /*------------------------ Cached Element References ------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
